572bd370a02674af9b18b96ac60c6ce9fefbc256
Dirk Eddelbuettel <edd@debian.org>
2009-01-03 17:10:33 +0000

    comitting current pre-0.2.0 snapshot

--- R/random.R
+++ R/random.R
@@ -1,70 +1,114 @@
 
-# $Id: random.R,v 1.2 2007/04/26 01:57:39 edd Exp $
+# $Id: random.R,v 1.3 2009/01/02 01:27:56 edd Exp $
 
-randomBufferStatus <- function() {
-  urltxt <- "http://www.random.org/cgi-bin/checkbuf"
-  checkBuf <- read.table(file=urltxt, as.is=TRUE)
-  ## strip trailing '%', and dividide by 100 so that '45%' becomes 0.45
-  pct <- as.numeric(gsub('%','', checkBuf))/100
-  names(pct) <- 'checkbuf'
-  return(pct)
+## randomBufferStatus <- function() {
+##   urltxt <- "http://www.random.org/cgi-bin/checkbuf"
+##   checkBuf <- read.table(file=urltxt, as.is=TRUE)
+##   ## strip trailing '%', and dividide by 100 so that '45%' becomes 0.45
+##   pct <- as.numeric(gsub('%','', checkBuf))/100
+##   names(pct) <- 'checkbuf'
+##   return(pct)
+## }
+
+## sufficientBits <- function() {
+##   ## following Haahr's suggestion, we compare to a level of 20\%
+##   return( randomBufferStatus() >= 0.2 )
+## }
+
+randomQuota <- function() {
+    urltxt <- "http://random.org/quota/?format=plain"
+    ##quota <- read.table(file=urltxt, as.is=TRUE, header=FALSE, nrows=1)
+    quota <- scan(file=urltxt, nlines=1, quiet=TRUE)
+    return(quota)
 }
 
-sufficientBits <- function() {
-  ## following Haahr's suggestion, we compare to a level of 20\%
-  return( randomBufferStatus() >= 0.2 )
+quotaCheck <- function() {
+    return(randomQuota() > 0)     	# ok as long as it is positive
 }
 
-randomNumbers <- function(n=100, min=1, max=100, col=5, check=TRUE) {
-  if (n < 1 || n > 10000)
+randomNumbers <- function(n=100, min=1, max=100, col=5, base=10, check=TRUE) {
+  if (n < 1 || n > 1e4)
     stop("Random number requests must be between 1 and 10,000 numbers")
-  if (min < -1000000000 | max > 1000000000)
+  if (min < -1e9 | max > 1e9 | min > max)
     stop("Random number range must be between -1000,000,000 and 1000,000,000")
   if (n %% col != 0)
     stop("Request size 'n' not cleanly divisible by column size 'col'")
-  if (check & n >= 1000 & !sufficientBits()) 
-    message("random.org suggests to wait with larger requests")
-  urlbase <- "http://random.org/cgi-bin/"
+  if (!base %in% c(2,8,10,16))
+    stop("Base has to be one of 2, 8, 10 or 16")
+  if (check & FALSE==quotaCheck())
+    stop("random.org suggests to wait until tomorrow")
+  urlbase <- "http://random.org/integers/"
   urltxt <- paste(urlbase,
-                  "randnum?num=", format(n, scientific=FALSE),
+                  "?num=", format(n, scientific=FALSE),
                   "&min=", format(min, scientific=FALSE),
                   "&max=", format(max, scientific=FALSE),
                   "&col=", col,
+                  "&base=", base,
+                  "&format=plain",
+                  "&rnd=new",
                   sep="")
   randNum <- as.matrix(read.table(file=urltxt, as.is=TRUE))
   return(randNum)
 }
 
 randomSequence <- function(min=1, max=20, check=TRUE) {
-  if (min < -1000000000 | max > 1000000000)
+  if (min < -1e9 | max > 1e9 | min > max)
     stop(paste("Random sequence range must be between -1000,000,000",
                "and 1000,000,000"))
-  if (check & (max-min+1) >= 1000 & !sufficientBits()) 
+  if (check & FALSE==quotaCheck())
     message("random.org suggests to wait with larger requests")
-  urlbase <- "http://random.org/cgi-bin/"
+  urlbase <- "http://random.org/sequences/"
   urltxt <- paste(urlbase,
-                  "randseq?min=", min,
+                  "?min=", min,
                   "&max=", max,
+                  "&format=plain",
+                  "&rnd=new",
                   sep="")
   randSeq <- as.matrix(read.table(file=urltxt, as.is=TRUE))
   return(randSeq)
 }
 
-randomBytes <- function(n=16, format=c("hex","dec","oct", "bin"), check=TRUE) {
-  if (n < 16 || n > 16384)
-    stop("Random bytes requests must be between 16 and 16,384 bytes")
-  if (n %% 16 !=0) 
-    stop("Random bytes request size must be divisible by 16")
+## randomBytes <- function(n=16, format=c("hex","dec","oct", "bin"), check=TRUE) {
+##   if (n < 16 || n > 16384)
+##     stop("Random bytes requests must be between 16 and 16,384 bytes")
+##   if (n %% 16 !=0)
+##     stop("Random bytes request size must be divisible by 16")
 
-  format <- match.arg(format)
-  
-  urlbase <- "http://random.org/cgi-bin/"
+##   format <- match.arg(format)
+
+##   urlbase <- "http://random.org/cgi-bin/"
+##   urltxt <- paste(urlbase,
+##                   "randbyte?nbytes=", n,
+##                   "&format=", format,
+##                   sep="")
+
+##   randBytes <- as.matrix(read.table(file=urltxt))
+##   return(randBytes)
+## }
+
+randomStrings <- function(n=10, len=5, digits=TRUE, upperalpha=TRUE, loweralpha=TRUE, unique=TRUE, check=TRUE) {
+  if (n < 1 || n > 1e4)
+    stop("Random string requests must be between 1 and 10,000 numbers")
+  if (len < 1 || len > 20)
+    stop("Random string length must be between 1 and 20")
+  if (class(digits)!="logical" || class(upperalpha)!="logical" || class(loweralpha)!="logical" || class(unique)!="logical")
+    stop("The 'digits', '(lower|upper)alpha' and 'unique' arguments has to be logical")
+  if ( !digits && !upperalpha && !loweralpha)
+    stop("The 'digits', 'loweralpha' and 'loweralpha' cannot all be false at the same time")
+  if (check & FALSE==quotaCheck())
+    stop("random.org suggests to wait until tomorrow")
+  urlbase <- "http://random.org/strings/"
   urltxt <- paste(urlbase,
-                  "randbyte?nbytes=", n,
-                  "&format=", format,
+                  "?num=", n,
+                  "&len=", len,
+                  "&digits=", ifelse(digits, "on", "off"),
+                  "&upperalpha=", ifelse(upperalpha, "on", "off"),
+                  "&loweralpha=", ifelse(loweralpha, "on", "off"),
+                  "&unique=", ifelse(unique, "on", "off"),
+                  "&format=plain",
+                  "&rnd=new",
                   sep="")
-
-  randBytes <- as.matrix(read.table(file=urltxt))
-  return(randBytes)
+  randStrings <- as.matrix(read.table(file=urltxt))
+  return(randStrings)
 }
 
