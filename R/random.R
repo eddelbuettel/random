c8627d0496eb5e1fdcc445a6aa9277c19bd9c5fe
Dirk Eddelbuettel <edd@debian.org>
2015-05-15 17:20:50 -0500

    new function to use internal url(..., method="libcurl") where available (R >= 3.2.0)

--- R/random.R
+++ R/random.R
@@ -18,9 +18,17 @@
 ##  You should have received a copy of the GNU General Public License
 ##  along with random.  If not, see <http://www.gnu.org/licenses/>.
 
+fetch <- function(urltxt, ...) {
+    if (getRversion() >= "3.2.0" && capabilities()["libcurl"]) {
+        url(urltxt, ..., method="libcurl")
+    } else {
+        curl(urltxt, ...)
+    }
+}
+
 randomQuota <- function() {
     urltxt <- "https://random.org/quota/?format=plain"
-    con <- curl(urltxt)
+    con <- fetch(urltxt)
     quota <- as.integer(readLines(con))
     close(con)
     return(quota)
@@ -51,7 +59,7 @@ randomNumbers <- function(n=100, min=1, max=100, col=5, base=10, check=TRUE) {
                     "&format=plain",
                     "&rnd=new",
                     sep="")
-    randNum <- as.matrix(read.table(curl(urltxt, open="r"), as.is=TRUE))
+    randNum <- as.matrix(read.table(fetch(urltxt, open="r"), as.is=TRUE))
     return(randNum)
 }
 
@@ -69,7 +77,7 @@ randomSequence <- function(min=1, max=20, col=1, check=TRUE) {
                     "&format=plain",
                     "&rnd=new",
                     sep="")
-    randSeq <- as.matrix(read.table(curl(urltxt, open="r"), as.is=TRUE))
+    randSeq <- as.matrix(read.table(fetch(urltxt, open="r"), as.is=TRUE))
     return(randSeq)
 }
 
@@ -98,6 +106,6 @@ randomStrings <- function(n=10, len=5, digits=TRUE,
                     "&format=plain",
                     "&rnd=new",
                     sep="")
-    randStrings <- as.matrix(read.table(curl(urltxt, open="r")))
+    randStrings <- as.matrix(read.table(fetch(urltxt, open="r")))
     return(randStrings)
 }
